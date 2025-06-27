04cae71edf928682b208a25cfe4ea7f723e3495b
Max <Maximilianlakke@googlemail.com>
2025-06-27 13:01:24 +0200

    add level logic

---
+++ bjs-2025/objects/lever.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var canTriggerLever: bool = false
+
+func _process(delta: float) -> void:
+	if canTriggerLever && Input.is_action_just_pressed("ACTION"):
+		$AnimatedSprite2D.play()
+		
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	canTriggerLever = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	canTriggerLever = false
