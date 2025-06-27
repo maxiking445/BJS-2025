d9a5c1cec55cfbae3f486fb7f34a2ed315697093
Max <Maximilianlakke@googlemail.com>
2025-06-27 11:35:02 +0200

    add shakytext

---
+++ bjs-2025/Enums.gd
@@ -0,0 +1,3 @@
+extends Node
+
+enum TEXT_DISPLAY{BACKWARDS, NORMAL, RANDOM}
---
+++ bjs-2025/UI/shaky_text.gd
@@ -0,0 +1,47 @@
+extends Control
+
+@export var shake_intensity: int = 2
+@export var text: String = "UNDEFINED"
+@export var lifeTime: float = 2
+@export var text_display: Enums.TEXT_DISPLAY = Enums.TEXT_DISPLAY.NORMAL
+var original_position = Vector2.ZERO
+
+func _ready() -> void:
+	$LifeTimer.wait_time = lifeTime
+	$LifeTimer.start()
+	
+	original_position = $Label.position
+	if text_display == Enums.TEXT_DISPLAY.BACKWARDS:
+		$Label.text = text.reverse()
+	elif text_display == Enums.TEXT_DISPLAY.RANDOM:
+		$Label.text = shuffle_string(text) 
+	else:
+		$Label.text = text
+
+
+func _process(delta: float) -> void:
+	var shake_offset = Vector2(
+		randf_range(-1.0, 1.0),
+		randf_range(-1.0, 1.0)
+	) * shake_intensity
+	$Label.position = original_position + shake_offset
+
+func shuffle_string(text: String) -> String:
+	var chars: PackedStringArray = text.split("")
+	var randomWord= "".join(shuffle_array(chars))
+	return randomWord
+
+func shuffle_array(arr: PackedStringArray) -> PackedStringArray:
+	var n = arr.size()
+	for i in range(n - 1, 0, -1):
+		var j = randi() % (i + 1)
+		var temp = arr[i]
+		arr[i] = arr[j]
+		arr[j] = temp
+	return arr
+
+
+func _on_life_timer_timeout() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_callback(self.queue_free)  # löscht das Label nach dem Ausblenden
